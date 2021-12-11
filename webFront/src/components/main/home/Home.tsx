ee8613658c7699be1a2dbe459068093f01c21c0f
goldfrosch <goldfrosch@daum.net>
2021-12-11 14:30:44 +0900

    GoldFrosch / front -> 홈 페이지 퍼블리싱 수정

--- webFront/src/components/main/home/Home.tsx
+++ webFront/src/components/main/home/Home.tsx
@@ -32,83 +32,86 @@ const Home: React.FC<HomeProps> = ({ data, list }) => {
         ))}
       </Slick>
       <div className="main">
-        <div className="items">
-          <HomeItem>
-            <div className="borderHead">
-              <span>공지사항</span>
-              <Link to="/board?category=notice">
-                <AddIcon />
-              </Link>
-            </div>
-            {list.noticeList.map((data, key) => (
-              <Link to={`/board/${data.id}`} key={key}>
-                <div className="borderBody" style={{ cursor: "pointer" }}>
-                  <span className="title">{data.title}</span>
-                  <span className="date">
-                    {DateUtils.getDay(data.createdDate)}
-                  </span>
-                </div>
-              </Link>
-            ))}
-            {list.noticeList.length < 4 &&
-              [...Array(4 - list.noticeList.length)].map((_, key) => (
-                <div className="borderBody" key={key}>
-                  빈 게시글
-                </div>
-              ))}
-          </HomeItem>
-          <HomeItem>
-            <div className="borderHead">
-              <span>업데이트</span>
-              <Link to="/board?category=update">
-                <AddIcon />
-              </Link>
-            </div>
-            {list.updateList.map((data, key) => (
-              <Link to={`/board/${data.id}`} key={key}>
-                <div className="borderBody">
-                  <span className="title">{data.title}</span>
-                  <span className="date">
-                    {DateUtils.getDay(data.createdDate)}
-                  </span>
-                </div>
-              </Link>
-            ))}
-            {list.updateList.length < 4 &&
-              [...Array(4 - list.updateList.length)].map((_, key) => (
-                <div className="borderBody" key={key}>
-                  빈 게시글
-                </div>
-              ))}
-          </HomeItem>
-        </div>
-      </div>
-
-      {/* <HomeItem>
-        <div className="borderHead">
-          <span>사진게시판</span>
-          <Link to="/grid?category=photo">
-            <AddIcon />
-          </Link>
-        </div>
-        <div className="gridBody">
-          {list.photoList.map((data, key) => (
-            <Link to={`/board/${data.id}`} key={key}>
-              <div className="gridItem">
-                <img
-                  src={
-                    data.thumbnail
-                      ? `http://moasv.co.kr/images/${data.thumbnail}`
-                      : Thumbnail
-                  }
-                  alt=""
-                />
-                <span>{data.title}</span>
+        <section>
+          <div className="items">
+            <HomeItem>
+              <div className="borderHead">
+                <span>공지사항</span>
+                <Link to="/board?category=notice">
+                  <AddIcon />
+                </Link>
               </div>
-            </Link>
-          ))}
-        </div>
-      </HomeItem> */}
+              {list.noticeList.map((data, key) => (
+                <Link to={`/board/${data.id}`} key={key}>
+                  <div className="borderBody" style={{ cursor: "pointer" }}>
+                    <span className="title">{data.title}</span>
+                    <span className="date">
+                      {DateUtils.getDay(data.createdDate)}
+                    </span>
+                  </div>
+                </Link>
+              ))}
+              {list.noticeList.length < 4 &&
+                [...Array(4 - list.noticeList.length)].map((_, key) => (
+                  <div className="borderBody" key={key}>
+                    빈 게시글
+                  </div>
+                ))}
+            </HomeItem>
+            <HomeItem>
+              <div className="borderHead">
+                <span>업데이트</span>
+                <Link to="/board?category=update">
+                  <AddIcon />
+                </Link>
+              </div>
+              {list.updateList.map((data, key) => (
+                <Link to={`/board/${data.id}`} key={key}>
+                  <div className="borderBody">
+                    <span className="title">{data.title}</span>
+                    <span className="date">
+                      {DateUtils.getDay(data.createdDate)}
+                    </span>
+                  </div>
+                </Link>
+              ))}
+              {list.updateList.length < 4 &&
+                [...Array(4 - list.updateList.length)].map((_, key) => (
+                  <div className="borderBody" key={key}>
+                    빈 게시글
+                  </div>
+                ))}
+            </HomeItem>
+          </div>
+          <div className="list">
+            <HomeItem>
+              <div className="borderHead">
+                <span>사진게시판</span>
+                <Link to="/grid?category=photo">
+                  <AddIcon />
+                </Link>
+              </div>
+              <div className="gridBody">
+                {list.photoList.map((data, key) => (
+                  <Link to={`/board/${data.id}`} key={key}>
+                    <div className="gridItem">
+                      <img
+                        src={
+                          data.thumbnail
+                            ? `http://moasv.co.kr/images/${data.thumbnail}`
+                            : Thumbnail
+                        }
+                        alt=""
+                      />
+                      <span>{data.title}</span>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </HomeItem>
+          </div>
+        </section>
+      </div>
     </HomeBlock>
   );
 };
@@ -121,27 +124,35 @@ const HomeBlock = styled.div`
   }
   & > .main {
     width: 100%;
-    max-width: 1080px;
-
-    @media (max-width: 1080px) {
-      max-width: 90%;
-      min-width: 750px;
-    }
-    @media (max-width: 768px) {
-      max-width: 100%;
-      min-width: 100%;
-    }
-    & > .items {
+    display: flex;
+    justify-content: center;
+    & > section {
       width: 100%;
+      max-width: 1280px;
 
-      padding: 16px 0;
-
-      display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(40%, auto));
-
-      grid-gap: 16px;
+      @media (max-width: 1280px) {
+        max-width: 90%;
+        min-width: 768px;
+      }
       @media (max-width: 768px) {
-        grid-template-columns: repeat(auto-fill, minmax(100%, auto));
+        max-width: 100%;
+        min-width: 100%;
+      }
+      & > .items {
+        width: 100%;
+
+        padding: 16px 0;
+
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(40%, auto));
+
+        grid-gap: 16px;
+        @media (max-width: 768px) {
+          grid-template-columns: repeat(auto-fill, minmax(100%, auto));
+        }
+      }
+      & > .list {
+        width: 100%;
       }
     }
   }
